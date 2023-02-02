455a8fbd955be679f9d279e1199cc55a19763f48
EC2 Default User <ec2-user@ip-172-31-3-220.ap-northeast-1.compute.internal>
2023-02-02 05:48:43 +0000

    update 非同期

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,13 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+   # 自分がフォローされる（被フォロー）側の関係性
    has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    # 被フォロー関係を通じて参照→自分をフォローしている人
    has_many :followers, through: :reverse_of_relationships, source: :follower
-   
+    # 被フォロー関係を通じて参照→自分をフォローしている人
    has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+   # 与フォロー関係を通じて参照→自分がフォローしている人
    has_many :followings, through: :relationships, source: :followed
   
   has_one_attached :profile_image
@@ -47,5 +49,5 @@ class User < ApplicationRecord
     else
       User.where('name LIKE ?', '%' + content + '%')
     end
-  end
+   end
 end
