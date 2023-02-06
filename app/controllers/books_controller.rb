d4ae7e7fc1c79b2bfbf9cb5795fd5566218e2a4b
EC2 Default User <ec2-user@ip-172-31-3-220.ap-northeast-1.compute.internal>
2023-02-06 06:51:38 +0000

    update コメント数

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,23 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
+    
+    @book_detail = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end
+
   end
 
   def index
     # @books = Book.all
     @book = Book.new
     @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    
+     @book_detail = Book.find_by(params[:book_id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,12 @@ class UsersController < ApplicationController
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
+    
+    
+    @book_detail = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end
   end
 
   def index
